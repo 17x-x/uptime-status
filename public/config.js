694c0972fdcd8b85e5caf01075810a35f6fcb4e7
17x <3303002880@qq.com>
2024-03-10 22:20:25 +0800

    2024-03-10

--- public/config.js
+++ public/config.js
@@ -7,11 +7,17 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2488818-23da2307a9b1a9fb7ae956f1',
+    'm796543052-7282cdbfe7296bcc09170b8f',
+    'm796542884-c772b2f8595b85be169c29fd',
+    'm796543060-df02a63525bf4abf4e50e43f',
+    'm796543062-838a9bd61205449b1bace56f',
+    'm796543055-9c93a35e6a6b7569fbb70620',
+    'm796543058-b843bb4f26df4c0402cce4bf',
+    'm796543046-61b912dbe46a0466432f7257',
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 60,
 
   // 是否显示检测站点的链接
   ShowLink: true,
