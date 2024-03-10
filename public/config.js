ad2d70b418116a38fb77abfbde6935606adb296b
17x <3303002880@qq.com>
2024-03-10 21:37:04 +0800

    2024-03-10

---
+++ public/config.js
@@ -0,0 +1,36 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'ZZM真TM帅',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2488818-23da2307a9b1a9fb7ae956f1',
+    'm784490063-7b5da437e7f1e0d67613714d',
+    'm784497419-de55aa09902ccb3ab22d548a',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
+  ],
+
+  // 日志天数
+  CountDays: 180,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
